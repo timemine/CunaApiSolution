63d4fab5caab3bc5d7db1c9029d36ee19b60e1fe
Amanda Green <timemine@hotmail.com>
2020-06-14 17:24:29 -0700

    Add project files.

---
+++ CunaApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CunaApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
